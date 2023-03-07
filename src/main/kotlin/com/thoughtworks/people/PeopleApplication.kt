abd494db5fc5f2fba245b2d43ef2320393002143
Sergey Bukharov <bukharovSI@gmail.com>
2023-03-07 20:49:38 +0800

    main class is open

--- src/main/kotlin/com/thoughtworks/people/PeopleApplication.kt
+++ src/main/kotlin/com/thoughtworks/people/PeopleApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class PeopleApplication
+open class PeopleApplication
 
 fun main(args: Array<String>) {
 	runApplication<PeopleApplication>(*args)
