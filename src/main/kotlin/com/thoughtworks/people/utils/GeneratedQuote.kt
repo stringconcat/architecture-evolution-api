9e089a09ddea8ab88189fcac3feb0f74ddef818b
Sergey Bukharov <bukharovSI@gmail.com>
2023-03-07 13:05:23 +0800

    Change quote generator to quotable.io

--- src/main/kotlin/com/thoughtworks/people/utils/GeneratedQuote.kt
+++ src/main/kotlin/com/thoughtworks/people/utils/GeneratedQuote.kt
@@ -1,30 +1,21 @@
 package com.thoughtworks.people.utils
 
-import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
 
 const val defaultQuote = "whoops, something went wrong"
 
 class GeneratedQuote {
 
-    private val getRandomUrl = "https://quote-garden.herokuapp.com/quotes/random"
+    private val getRandomUrl = "https://api.quotable.io/random"
 
-    private val httpRequestFactory = SimpleClientHttpRequestFactory()
-            .also { it.setConnectTimeout(5000) }
-            .also { it.setReadTimeout(5000) }
 
-    fun get() =
-            try {
-                RestTemplate(httpRequestFactory)
-                        .getForEntity(getRandomUrl, QuoteResponse::class.java)
-                        .body?.quoteText ?: defaultQuote
-            } catch (e: Exception) {
-                defaultQuote
-            }
+    fun get() = RestTemplate()
+            .getForEntity(getRandomUrl, QuoteResponse::class.java)
+            .body?.content ?: defaultQuote
 
     internal data class QuoteResponse(
-            val _id: String,
-            val quoteText: String,
-            val quoteAuthor: String
+        val _id: String,
+        val content: String,
+        val author: String
     )
 }
