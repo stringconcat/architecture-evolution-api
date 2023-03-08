1287400fa59e06e6f3c7435d1cf7b6e9bf145178
Sergey Bukharov <bukharovSI@gmail.com>
2023-03-08 09:51:12 +0800

    add some complication for Person

--- src/main/kotlin/com/thoughtworks/people/model/Person.kt
+++ src/main/kotlin/com/thoughtworks/people/model/Person.kt
@@ -1,22 +1,50 @@
 package com.thoughtworks.people.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDate
+import java.time.Period
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.validation.constraints.NotEmpty
 
 @Entity
 data class Person(
         @Id val id: UUID = UUID.randomUUID(),
+        @JsonIgnore
+        @NotEmpty
         val firstName: String,
+        @JsonIgnore
+        @NotEmpty
         val secondName: String,
+        @JsonIgnore
         val birthDate: LocalDate,
-        @JsonProperty("gender") val sex: Sex,
+        @JsonProperty("gender")
+        val sex: Sex,
+        @NotEmpty
         val avatartUrl: String,
+        @NotEmpty
         val favoriteQuote: String
 ) {
     enum class Sex {
         MAN, WOMAN
     }
+
+    @JsonProperty("title")
+    fun title(): String {
+        val prefix = if (Period.between(LocalDate.now(), birthDate).years > 40) {
+            when (sex) {
+                Sex.MAN -> "Mr. "
+                Sex.WOMAN -> "Mrs. "
+            }
+        } else {
+            ""
+        }
+
+        return "$prefix$firstName $secondName"
+    }
+
+    @JsonProperty("birthday")
+    fun birthday() = "${birthDate.month.name} ${birthDate.year}"
 }
