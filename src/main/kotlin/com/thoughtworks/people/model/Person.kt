175e4ccfe7da83b0f0548ae6ee46350d572e2395
sergei <sergei.bukharov@tech.jago.com>
2025-07-09 18:07:38 +0800

    update project

--- src/main/kotlin/com/thoughtworks/people/model/Person.kt
+++ src/main/kotlin/com/thoughtworks/people/model/Person.kt
@@ -1,29 +1,52 @@
 package com.thoughtworks.people.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.validation.constraints.NotEmpty
 import java.time.LocalDate
 import java.time.Period
 import java.util.*
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.validation.constraints.NotEmpty
 
 @Entity
 data class Person(
-        @Id val id: UUID = UUID.randomUUID(),
+    @Id val id: UUID = UUID.randomUUID(),
+    @JsonIgnore
         @NotEmpty
         val firstName: String,
+    @JsonIgnore
         @NotEmpty
         val secondName: String,
+    @JsonIgnore
         val birthDate: LocalDate,
+    @JsonProperty("gender")
         val sex: Sex,
-        @NotEmpty
+    @NotEmpty
         val avatartUrl: String,
-        @NotEmpty
+    @NotEmpty
         val favoriteQuote: String
 ) {
     enum class Sex {
         MAN, WOMAN
     }
 
+    @JsonProperty("title")
+    fun title(): String {
+        val prefix = if (Period.between(LocalDate.now(), birthDate).years > 40) {
+            when (sex) {
+                Sex.MAN -> "Mr. "
+                Sex.WOMAN -> "Mrs. "
+            }
+        } else {
+            ""
+        }
+
+        return "$prefix$firstName $secondName"
+    }
+
     fun ageYears() = Period.between(birthDate, LocalDate.now()).years
+
+    @JsonProperty("birthday")
+    fun birthday() = "${birthDate.month.name} ${birthDate.year}"
 }
