1d8e7b077c6eabb4b730d19d101a17611774a2d5
sergei <sergei.bukharov@tech.jago.com>
2025-07-10 16:06:58 +0800

    remove some unnecessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
-
 plugins {
-//	id("buildsrc.convention.kotlin-jvm")
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25" apply false
 	id("org.springframework.boot") version "3.5.3" apply false
@@ -18,9 +16,6 @@ java {
 }
 
 allprojects {
-//	apply {
-//		plugin("buildsrc.convention.kotlin-jvm")
-//	}
 
 	repositories {
 		gradlePluginPortal()
@@ -34,19 +29,10 @@ repositories {
 }
 
 dependencies {
-
 	implementation(project(":application"))
-	implementation(libs.springBootStarter)
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation(libs.springBootStarterWeb)
-	implementation(libs.springBootStarterDataJpa)
-	implementation(libs.springBootStarterValidation)
 
-
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	runtimeOnly("com.h2database:h2:2.2.224")
 
 }
 
